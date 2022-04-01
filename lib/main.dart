0d331c35cca1621e63ea3a1cc3f4806597b2764e
FlutLab.io <>
2022-04-01 02:45:00 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatediniciowidget/GeneratedInicioWidget.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatednuevousuariowidget1/GeneratedNuevousuarioWidget1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedpaginaprincipalwidget/GeneratedPaginaprincipalWidget.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generateddrawerwidget/GeneratedDrawerWidget.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedperfilwidget2/GeneratedPerfilWidget2.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedconfiguracionwidget1/GeneratedConfiguracionWidget1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedppventanawidget/GeneratedPpventanaWidget.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedparatiwidget/GeneratedParatiWidget.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatednuevowidget2/GeneratedNuevoWidget2.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedppwidget1/GeneratedPpWidget1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generateddrivefolderuploadwidget/GeneratedDrivefolderuploadWidget.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedclouduploadwidget/GeneratedClouduploadWidget.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedobformsestoydeacuerdoyheledolostrminosycondicioneswidge1/GeneratedOBFormsEstoydeacuerdoyheledolostrminosycondicionesWidge1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedobformsnombredeusuariowidget1/GeneratedOBFormsNombredeusuarioWidget1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedobformscontraseawidget2/GeneratedOBFormsContraseaWidget2.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedobformscorreoelctronicowidget1/GeneratedOBFormsCorreoelctronicoWidget1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedobformsmantenersesininiciadawidget1/GeneratedOBFormsMantenersesininiciadaWidget1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedobformsusuariowidget1/GeneratedOBFormsUsuarioWidget1.dart';
+import 'package:flutterapp/rivasandrade6_i_20damovilesandroidapp/generatedobformscontraseawidget3/GeneratedOBFormsContraseaWidget3.dart';
+
+void main() {
+  runApp(RivasAndrade6_i_20DAMovilesAndroidApp());
+}
+
+class RivasAndrade6_i_20DAMovilesAndroidApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tetos Cars',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedInicioWidget',
+      routes: {
+        '/GeneratedInicioWidget': (context) => GeneratedInicioWidget(),
+        '/GeneratedNuevousuarioWidget1': (context) => GeneratedNuevousuarioWidget1(),
+        '/GeneratedPaginaprincipalWidget': (context) => GeneratedPaginaprincipalWidget(),
+        '/GeneratedDrawerWidget': (context) => GeneratedDrawerWidget(),
+        '/GeneratedPerfilWidget2': (context) => GeneratedPerfilWidget2(),
+        '/GeneratedConfiguracionWidget1': (context) => GeneratedConfiguracionWidget1(),
+        '/GeneratedPpventanaWidget': (context) => GeneratedPpventanaWidget(),
+        '/GeneratedParatiWidget': (context) => GeneratedParatiWidget(),
+        '/GeneratedNuevoWidget2': (context) => GeneratedNuevoWidget2(),
+        '/GeneratedPpWidget1': (context) => GeneratedPpWidget1(),
+        '/GeneratedDrivefolderuploadWidget': (context) => GeneratedDrivefolderuploadWidget(),
+        '/GeneratedClouduploadWidget': (context) => GeneratedClouduploadWidget(),
+        '/GeneratedOBFormsEstoydeacuerdoyheledolostrminosycondicionesWidge1': (context) => GeneratedOBFormsEstoydeacuerdoyheledolostrminosycondicionesWidge1(),
+        '/GeneratedOBFormsNombredeusuarioWidget1': (context) => GeneratedOBFormsNombredeusuarioWidget1(),
+        '/GeneratedOBFormsContraseaWidget2': (context) => GeneratedOBFormsContraseaWidget2(),
+        '/GeneratedOBFormsCorreoelctronicoWidget1': (context) => GeneratedOBFormsCorreoelctronicoWidget1(),
+        '/GeneratedOBFormsMantenersesininiciadaWidget1': (context) => GeneratedOBFormsMantenersesininiciadaWidget1(),
+        '/GeneratedOBFormsUsuarioWidget1': (context) => GeneratedOBFormsUsuarioWidget1(),
+        '/GeneratedOBFormsContraseaWidget3': (context) => GeneratedOBFormsContraseaWidget3(),
+      },
+    );
+  }
+}
